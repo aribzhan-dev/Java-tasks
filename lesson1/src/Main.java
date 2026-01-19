3d9327d1a65482fc429494807d312d2097e71fdc
Aribzhan <aribzankamilzanov@gmail.com>
2026-01-19 15:24:51 +0500

    new fix

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -390,19 +390,175 @@ public class Main {
 
         // task 19
 
-        System.out.print("Enter a number -->");
-        int a = scanner.nextInt();
-        System.out.print("Enter a number -->");
-        int b = scanner.nextInt();
-        System.out.print("Enter a number -->");
-        int c = scanner.nextInt();
-        if (a == b && a == c){
-            System.out.println("equilateral");
-        }else if (b == c || c == a || a == b){
-            System.out.println("isosceles");
-        }else{
-            System.out.println("scalene");
-        }
+//        System.out.print("Enter a number -->");
+//        int a = scanner.nextInt();
+//        System.out.print("Enter a number -->");
+//        int b = scanner.nextInt();
+//        System.out.print("Enter a number -->");
+//        int c = scanner.nextInt();
+//        if (a == b && a == c){
+//            System.out.println("equilateral");
+//        }else if (b == c || c == a || a == b){
+//            System.out.println("isosceles");
+//        }else{
+//            System.out.println("scalene");
+//        }
+
+        // task 20
+//        System.out.print("Enter a number -->");
+//        int x = scanner.nextInt();
+//        if (x > 0) {
+//            if (x % 2 == 0) {
+//                System.out.println("positive even");
+//            }else {
+//                System.out.println("positive odd");
+//            }
+//        }else{
+//            System.out.println("non-positive");
+//        }
+
+
+
+        // B) switch
+
+        // task 1
+
+//        int day = scanner.nextInt();
+//
+//        switch (day) {
+//            case 1 -> System.out.println("Monday");
+//            case 2 -> System.out.println("Tuesday");
+//            case 3 -> System.out.println("Wednesday");
+//            case 4 -> System.out.println("Thursday");
+//            case 5 -> System.out.println("Friday");
+//            case 6 -> System.out.println("Saturday");
+//            case 7 -> System.out.println("Sunday");
+//            default -> System.out.println("Invalid day");
+//        }
+
+        // task 2
+
+//        int month = scanner.nextInt();
+//
+//        switch (month) {
+//            case 1 -> System.out.println("January");
+//            case 2 -> System.out.println("February");
+//            case 3 -> System.out.println("March");
+//            case 4 -> System.out.println("April");
+//            case 5 -> System.out.println("May");
+//            case 6 -> System.out.println("June");
+//            case 7 -> System.out.println("July");
+//            case 8 -> System.out.println("August");
+//            case 9 -> System.out.println("September");
+//            case 10 -> System.out.println("October");
+//            case 11 -> System.out.println("November");
+//            case 12 -> System.out.println("December");
+//            default -> System.out.println("Invalid month");
+//        }
+
+        // task 3
+
+//        int mark = scanner.nextInt();
+//
+//        switch (mark) {
+//            case 1 -> System.out.println("very bad");
+//            case 2 -> System.out.println("bad");
+//            case 3 -> System.out.println("average");
+//            case 4 -> System.out.println("good");
+//            case 5 -> System.out.println("excellent");
+//            default -> System.out.println("Invalid mark");
+//        }
+
+
+        // task 4
+
+//        char op = scanner.next().charAt(0);
+//        float a = scanner.nextFloat();
+//        float b = scanner.nextFloat();
+//
+//        switch (op) {
+//            case '+' -> System.out.println(a + b);
+//            case '-' -> System.out.println(a - b);
+//            case '*' -> System.out.println(a * b);
+//            case '/' -> System.out.println(b != 0 ? a / b : "Division by zero");
+//            default -> System.out.println("Unknown operator");
+//        }
+
+        // task 5
+//        char ch = scanner.next().toLowerCase().charAt(0);
+//
+//        switch (ch) {
+//            case 'a','e','i','o','u' -> System.out.println("vowel");
+//            default -> System.out.println("consonant/other");
+//        }
+
+
+        // task 6
+//        int n = scanner.nextInt();
+//
+//        switch (n) {
+//            case 0 -> System.out.println("zero");
+//            case 1 -> System.out.println("one");
+//            case 2 -> System.out.println("two");
+//            case 3 -> System.out.println("three");
+//            case 4 -> System.out.println("four");
+//            case 5 -> System.out.println("five");
+//            case 6 -> System.out.println("six");
+//            case 7 -> System.out.println("seven");
+//            case 8 -> System.out.println("eight");
+//            case 9 -> System.out.println("nine");
+//            default -> System.out.println("Invalid number");
+//        }
+
+
+        // task 7
+//        int dir = scanner.nextInt();
+//
+//        switch (dir) {
+//            case 1 -> System.out.println("North");
+//            case 2 -> System.out.println("East");
+//            case 3 -> System.out.println("South");
+//            case 4 -> System.out.println("West");
+//            default -> System.out.println("Invalid direction");
+//        }
+
+        // task 8
+
+//        int code = scanner.nextInt();
+//
+//        switch (code) {
+//            case 200 -> System.out.println("OK");
+//            case 404 -> System.out.println("Not Found");
+//            case 500 -> System.out.println("Server Error");
+//            default -> System.out.println("Unknown");
+//        }
+
+        // task 9
+
+//        char grade = scanner.next().toUpperCase().charAt(0);
+//
+//        switch (grade) {
+//            case 'A' -> System.out.println("90–100");
+//            case 'B' -> System.out.println("80–89");
+//            case 'C' -> System.out.println("70–79");
+//            case 'D' -> System.out.println("60–69");
+//            case 'F' -> System.out.println("0–59");
+//            default -> System.out.println("Invalid grade");
+//        }
+
+        // task 10
+
+//        int m = scanner.nextInt();
+//
+//        switch (m) {
+//            case 12,1,2 -> System.out.println("Winter");
+//            case 3,4,5 -> System.out.println("Spring");
+//            case 6,7,8 -> System.out.println("Summer");
+//            case 9,10,11 -> System.out.println("Autumn");
+//            default -> System.out.println("Invalid month");
+//        }
+
+
 
 
 
