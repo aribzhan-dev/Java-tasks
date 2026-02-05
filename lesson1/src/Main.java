56aee0b8cba5878a9c52444e75413102578bb7ca
Aribzhan <aribzankamilzanov@gmail.com>
2026-02-05 13:59:17 +0500

    new solved task

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -1683,41 +1683,181 @@ public class Main{
 
         // Task 2
 
+//         int a=4;
+//         int[][]b=new int[a][a];
+//         int num=1;
+//         int top=0,buttom=a-1,left=0,right=a-1;
+//         while(num<=a*a){
+//             for(int i=left;i<=right;i++){
+//                 b[top][i]=num++;
+//             }
+//             top++;
+//             for(int i=top;i<=buttom;i++){
+//                 b[i][right]=num++;
+//             }
+//             right--;
+//             for(int i=right;i>=left;i--){
+//                 b[buttom][i]=num++;
+//             }
+//             buttom--;
+//             for(int i=buttom;i>=top;i--){
+//                 b[i][left]=num++;
+//             }
+//             left++;
+//         }
+
+
+        // task 3
+
+//         int a = 5;
+//         int[][] b = new int[a][a];
+//         int num = 1;
+//         for(int s = 0; s <= 2 * (a-1); s++){
+//             if(s % 2 == 0){
+//                 for(int i = 0; i < a; i++){
+//                     int j = s - i;
+//                     if(j >= 0 && j < a){
+//                         b[i][j] = num++;
+//                     }
+//                 }
+//             }else{
+//                 for(int i = a-1; i >= 0; i--){
+//                     int j = s - i;
+//                     if(j >= 0 && j < a){
+//                         b[i][j] = num++;
+//                     }
+//                 }
+//             }
+//         }
+//         for(int i = 0; i < a; i++){
+//             for(int j = 0; j < a; j++){
+//                 System.out.print(b[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
 
 
 
-        // block 3
-        // task 1
+        // task 4
 
-//        String s = "ababab";
-//        int len_s = s.length();
-//        int mix_len = 0;
-//        for (int k=1; k <= len_s; k++){
-//            if (len_s % k == 0){
-//                int k_counter = 0;
-//                for(int i=k; i < len_s; i++){
-//                    if(s.charAt(i) == s.charAt(i % k)){
-//                        k_counter += 1;
+//         int a = 7;
+//         int mid = a / 2;
+//         int[][] b = new int[a][a];
+//         for(int i = 0; i < a; i++){
+//             int s;
+//             if(i <= mid){
+//                 s = 1 + 2 * i;
+//             }else{
+//                 s = 1 + 2 * (a - i - 1);
+//             }
+//             int d = (a - s) / 2;
+//             for(int j = 0; j < d; j++){
+//                 b[i][j] = '.';
+//             }
+//             for(int j = d; j < s; j++){
+//                 b[i][j] = '#';
+//             }
+//             for(int j = d + s; j < d; j++){
+//                 b[i][j] = '.';
+//             }
+//         }
+//         for(int i = 0; i < a; i++){
+//             for(int j = 0; j < a; j++){
+//                 System.out.print((char)b[i][j]);
+//             }
+//             System.out.println();
+//         }
+
+
+
+        // task 5
+
+//         int a = 20;
+//         int[][] b = new int[a][a];
+//         for(int i = 0; i < a; i++){
+//             for(int j = 0; j < a; j++){
+//                 b[i][j] = Math.min(Math.min(i, j),Math.min(a - 1 - i, a - 1 -j));
+//                 System.out.print(b[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+
+
+
+        // task 6
+
+//         int a = 20;
+//         char[][] b = new char[a][a];
+//         for(int i = 0; i < a; i++){
+//             for(int j = 0; j < a; j++){
+//                 if(i <= j && i + j < a){
+//                     b[i][j] = '1';
+//                 }
+//                 else if(i < j && i + j >= a){
+//                     b[i][j] = '2';
+//                 }
+//                 else if(i >= j && i + j >= a){
+//                     b[i][j] = '3';
+//                 }
+//                 else{
+//                     b[i][j] = '4';
+//                 }
 //
-//                    }
-//
-//                }
-//                if(k_counter == len_s-k){
-//                    mix_len = k;
-//                    break;
-//                }
+//             }
+//         }
+//         for(int i = 0; i < a; i++){
+//             for(int j = 0; j < a; j++){
+//                 System.out.print(b[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+
+
+
+         // task 7
+
+//         int a=20;
+//         int[][] b = new int[a][a];
+//         for(int i = 0; i < a; i++){
+//             for(int j = 0; j < a; j++){
+//                 b[i][j]=(i+1) * (j+1);
+//             }
+//         }
+//         int max=a * a;
+//         int width=String.valueOf(max).length();
+//         for(int i = 0; i < a; i++){
+//             for(int j = 0; j < a; j++){
+//                 System.out.printf("%" + width + "d", b[i][j]);
+//                 if(j != a - 1){
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+
+
+
+
+
+        // task 8
+
+//        int a = 20;
+//        int[][] b = new int[a][a];
+//        int d = a / 2;
+//        for(int i = 0; i < a; i++){
+//            for(int j = 0; j < a; j++){
+//                int s=Math.max(Math.abs(i - d),Math.abs(j - d));
+//                b[i][j]=d - s;
 //            }
 //        }
-//        if (mix_len == 0){
-//            System.out.println(len_s);
-//        }else{
-//            System.out.println(mix_len);
+//        for(int i = 0; i < a; i++){
+//            for(int j = 0; j < a; j++){
+//                System.out.print(b[i][j] + " ");
+//            }
+//            System.out.println();
 //        }
 
 
-        // task 2
-
-//        System.out.println(calculateUniqueLength(2, 3, 2, 7));
 
 
 
@@ -1844,9 +1984,38 @@ public class Main{
 
 
 
+        // block 3
+        // task 1
+
+//        String s = "ababab";
+//        int len_s = s.length();
+//        int mix_len = 0;
+//        for (int k=1; k <= len_s; k++){
+//            if (len_s % k == 0){
+//                int k_counter = 0;
+//                for(int i=k; i < len_s; i++){
+//                    if(s.charAt(i) == s.charAt(i % k)){
+//                        k_counter += 1;
+//
+//                    }
+//
+//                }
+//                if(k_counter == len_s-k){
+//                    mix_len = k;
+//                    break;
+//                }
+//            }
+//        }
+//        if (mix_len == 0){
+//            System.out.println(len_s);
+//        }else{
+//            System.out.println(mix_len);
+//        }
 
 
+        // task 2
 
+//        System.out.println(calculateUniqueLength(2, 3, 2, 7));
 
 
 
