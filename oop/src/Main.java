0c105af1040b986c9c75b35da09c5ebf25d18f68
Aribzhan <aribzankamilzanov@gmail.com>
2026-02-25 13:54:25 +0500

    new fix

--- oop/src/Main.java
+++ oop/src/Main.java
@@ -108,239 +108,343 @@
 
 // Delivery Service
 
-import java.util.*;
+//import java.util.*;
+//
+//
+//class Courier {
+//    private String name;
+//    private String surname;
+//    private String phone;
+//    private String transportType;
+//    private double rating;
+//
+//
+//    public Courier (String name, String surname, String phone, String transportType,
+//                   double rating){
+//        this.name = name;
+//        this.surname = surname;
+//        this.phone = phone;
+//        this.transportType = transportType;
+//        this.rating = rating;
+//    }
+//
+//
+//    public String getName() {
+//        return name + " " + surname;
+//    }
+//
+//    public String getInfo() {
+//        return name + " - " + transportType + " (" + rating + ")";
+//    }
+//
+//}
+//
+//class Customer {
+//    private String name;
+//    private String surname;
+//    private String address;
+//    private String phone;
+//
+//
+//    public Customer(String name, String surname, String address, String phone){
+//        this.name = name;
+//        this.surname = surname;
+//        this.address = address;
+//        this.phone = phone;
+//    }
+//
+//
+//    public String getName() {
+//        return name + " " + surname;
+//    }
+//
+//    public String getInfo(){
+//        return name + " " + address + " tel:" + phone;
+//    }
+//
+//}
+//
+//enum DeliveryStatus {
+//    CREATED,
+//    ON_THE_WAY,
+//    DELIVERED
+//}
+//
+//class DeliveryPackage{
+//    private int id;
+//    private double weight;
+//    private String fromAddress;
+//    private String toAddress;
+//    private DeliveryStatus status;
+//
+//
+//
+//    public DeliveryPackage(int id, double weight, String fromAddress, String toAddress){
+//        this.id = id;
+//        this.weight = weight;
+//        this.fromAddress = fromAddress;
+//        this.toAddress = toAddress;
+//        this.status = DeliveryStatus.CREATED;
+//    }
+//
+//    public void setStatus(DeliveryStatus status){
+//        this.status = status;
+//    }
+//
+//    public String getInfo(){
+//        return "id: " + id + " weight: " + weight + " ---- " + fromAddress + " --->" + toAddress + " status: " + status;
+//    }
+//
+//    public DeliveryStatus getStatus() {
+//        return status;
+//    }
+//
+//}
+//
+//
+//
+//class Delivery {
+//    private DeliveryPackage deliveryPackage;
+//    private Courier courier;
+//    private Customer customer;
+//
+//
+//    public Delivery(DeliveryPackage deliveryPackage, Courier courier, Customer customer){
+//        this.deliveryPackage = deliveryPackage;
+//        this.courier = courier;
+//        this.customer = customer;
+//    }
+//
+//    public void startDelivery(){
+//        deliveryPackage.setStatus(DeliveryStatus.ON_THE_WAY);
+//    }
+//
+//    public void finishDelivery(){
+//        deliveryPackage.setStatus(DeliveryStatus.DELIVERED);
+//
+//    }
+//
+//    public String getInfo(){
+//        return courier.getInfo() + " is delivering || " + deliveryPackage.getInfo() + " to " + customer.getInfo();
+//    }
+//
+//    public DeliveryStatus getStatus() {
+//        return deliveryPackage.getStatus();
+//    }
+//
+//
+//}
+//
+//
+//class DeliveryHistory{
+//    private List<Delivery> history = new ArrayList<>();
+//
+//
+//    public void addToHistory(Delivery delivery){
+//        if(delivery.getStatus() == DeliveryStatus.DELIVERED){
+//            history.add(delivery);
+//        } else {
+//            System.out.println("Delivery is not finished yet.");
+//        }
+//    }
+//
+//    public void showHistory(){
+//        for (Delivery d: history){
+//            System.out.println(d.getInfo());
+//        }
+//    }
+//}
+//
+//
+//class DeliveryService {
+//
+//    private List<Delivery> deliveries = new ArrayList<>();
+//    private DeliveryHistory history = new DeliveryHistory();
+//
+//    public void addDelivery(Delivery delivery){
+//        deliveries.add(delivery);
+//    }
+//
+//
+//    public void startDelivery(Delivery delivery){
+//        delivery.startDelivery();
+//    }
+//
+//    public void finishDelivery(Delivery delivery){
+//        delivery.finishDelivery();
+//        history.addToHistory(delivery);
+//    }
+//
+//    public void allDeliveries(){
+//        for(Delivery d : deliveries){
+//            System.out.println(d.getInfo());
+//        }
+//    }
+//
+//    public void showHistory(){
+//        history.showHistory();
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//class Main {
+//    public static void main(String[] args) {
+//
+//
+//        Customer cus1 = new Customer(
+//                "Aribzhan",
+//                "Kamilzhanov",
+//                "Mangilik EL C1",
+//                "+77079928009"
+//        );
+//
+//        Courier cour1 = new Courier(
+//                "Zair",
+//                "Shukhratov",
+//                "+77756270762",
+//                "Car",
+//                4.8
+//        );
+//
+//        DeliveryPackage pack1 = new DeliveryPackage(
+//                1,
+//                3.5,
+//                "Turkestan 10",
+//                "Mangilik El 10"
+//        );
+//
+//        DeliveryPackage pack2 = new DeliveryPackage(
+//                2,
+//                5.5,
+//                "Turkestan 13",
+//                "Mangilik El 35"
+//        );
+//
+//        Delivery d1 = new Delivery(pack1, cour1, cus1);
+//        Delivery d2 = new Delivery(pack2, cour1, cus1);
+//        d1.startDelivery();
+//        d2.finishDelivery();
+//
+//
+//        DeliveryService delSer = new DeliveryService();
+//        delSer.addDelivery(d1);
+//        delSer.allDeliveries();
+//
+//        DeliveryHistory delHis = new DeliveryHistory();
+//        delHis.addToHistory(d2);
+//        delHis.showHistory();
+//
+//
+//
+//
+//    }
+//}
 
 
-class Courier {
-    private String name;
-    private String surname;
-    private String phone;
-    private String transportType;
-    private double rating;
-
-
-    public Courier (String name, String surname, String phone, String transportType,
-                   double rating){
-        this.name = name;
-        this.surname = surname;
-        this.phone = phone;
-        this.transportType = transportType;
-        this.rating = rating;
-    }
-
-
-    public String getName() {
-        return name + " " + surname;
-    }
-
-    public String getInfo() {
-        return name + " - " + transportType + " (" + rating + ")";
-    }
-
-}
-
-class Customer {
-    private String name;
-    private String surname;
-    private String address;
-    private String phone;
-
-
-    public Customer(String name, String surname, String address, String phone){
-        this.name = name;
-        this.surname = surname;
-        this.address = address;
-        this.phone = phone;
-    }
-
-
-    public String getName() {
-        return name + " " + surname;
-    }
-
-    public String getInfo(){
-        return name + " " + address + " tel:" + phone;
-    }
-
-}
-
-enum DeliveryStatus {
-    CREATED,
-    ON_THE_WAY,
-    DELIVERED
-}
-
-class DeliveryPackage{
-    private int id;
-    private double weight;
-    private String fromAddress;
-    private String toAddress;
-    private DeliveryStatus status;
 
 
 
-    public DeliveryPackage(int id, double weight, String fromAddress, String toAddress){
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+class Order {
+    int id;
+    String services;
+    int price;
+
+    public Order(int id, String services, int price) {
         this.id = id;
-        this.weight = weight;
-        this.fromAddress = fromAddress;
-        this.toAddress = toAddress;
-        this.status = DeliveryStatus.CREATED;
+        this.services = services;
+        this.price = price;
     }
 
-    public void setStatus(DeliveryStatus status){
-        this.status = status;
-    }
-
-    public String getInfo(){
-        return "id: " + id + " weight: " + weight + " ---- " + fromAddress + " --->" + toAddress + " status: " + status;
-    }
-
-    public DeliveryStatus getStatus() {
-        return status;
-    }
-
-}
-
-
-
-class Delivery {
-    private DeliveryPackage deliveryPackage;
-    private Courier courier;
-    private Customer customer;
-
-
-    public Delivery(DeliveryPackage deliveryPackage, Courier courier, Customer customer){
-        this.deliveryPackage = deliveryPackage;
-        this.courier = courier;
-        this.customer = customer;
-    }
-
-    public void startDelivery(){
-        deliveryPackage.setStatus(DeliveryStatus.ON_THE_WAY);
-    }
-
-    public void finishDelivery(){
-        deliveryPackage.setStatus(DeliveryStatus.DELIVERED);
-
-    }
-
-    public String getInfo(){
-        return courier.getInfo() + " is delivering || " + deliveryPackage.getInfo() + " to " + customer.getInfo();
-    }
-
-    public DeliveryStatus getStatus() {
-        return deliveryPackage.getStatus();
-    }
-
-
-}
-
-
-class DeliveryHistory{
-    private List<Delivery> history = new ArrayList<>();
-
-
-    public void addToHistory(Delivery delivery){
-        if(delivery.getStatus() == DeliveryStatus.DELIVERED){
-            history.add(delivery);
-        } else {
-            System.out.println("Delivery is not finished yet.");
-        }
-    }
-
-    public void showHistory(){
-        for (Delivery d: history){
-            System.out.println(d.getInfo());
-        }
+    public void printOrder() {
+        System.out.println("Подписка #" + id + ": " + services + ", цена: " + price);
     }
 }
 
+class Subscription extends Order {
+    LocalDate startDate;
+    LocalDate endDate;
+    List<DayOfWeek> daysOfWeek;
 
-class DeliveryService {
-
-    private List<Delivery> deliveries = new ArrayList<>();
-    private DeliveryHistory history = new DeliveryHistory();
-
-    public void addDelivery(Delivery delivery){
-        deliveries.add(delivery);
+    public Subscription(int id, String services, int price, LocalDate startDate, LocalDate endDate, List<DayOfWeek> daysOfWeek) {
+        super(id, services, price);
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.daysOfWeek = daysOfWeek;
     }
 
-
-    public void startDelivery(Delivery delivery){
-        delivery.startDelivery();
+    public void extendSubscription(int extraDays) {
+        endDate = endDate.plusDays(extraDays);
+        System.out.println("Подписка #" + id + " продлена до " + endDate);
     }
 
-    public void finishDelivery(Delivery delivery){
-        delivery.finishDelivery();
-        history.addToHistory(delivery);
+    public void recalcPrice(int dailyPrice) {
+        int days = endDate.getDayOfYear() - startDate.getDayOfYear() + 1;
+        this.price = dailyPrice * daysOfWeek.size() * days;
     }
 
-    public void allDeliveries(){
-        for(Delivery d : deliveries){
-            System.out.println(d.getInfo());
-        }
-    }
-
-    public void showHistory(){
-        history.showHistory();
+    @Override
+    public void printOrder() {
+        System.out.println("Подписка #" + id + ": " + services + ", цена: " + price +
+                ", с " + startDate + " по " + endDate + ", дни: " + daysOfWeek);
     }
 }
 
-
-
-
-
-
-
-class Main {
+public class Main {
     public static void main(String[] args) {
+        List<Order> subscriptions = new ArrayList<>();
 
+        List<DayOfWeek> gymDays = new ArrayList<>();
+        gymDays.add(DayOfWeek.MONDAY);
+        gymDays.add(DayOfWeek.WEDNESDAY);
+        gymDays.add(DayOfWeek.FRIDAY);
 
-        Customer cus1 = new Customer(
-                "Aribzhan",
-                "Kamilzhanov",
-                "Mangilik EL C1",
-                "+77079928009"
-        );
-
-        Courier cour1 = new Courier(
-                "Zair",
-                "Shukhratov",
-                "+77756270762",
-                "Car",
-                4.8
-        );
-
-        DeliveryPackage pack1 = new DeliveryPackage(
+        Subscription s1 = new Subscription(
                 1,
-                3.5,
-                "Turkestan 10",
-                "Mangilik El 10"
+                "качалка",
+                0,
+                LocalDate.of(2026, 2, 24),
+                LocalDate.of(2026, 3, 24),
+                gymDays
         );
+        s1.recalcPrice(500);
+        subscriptions.add(s1);
 
-        DeliveryPackage pack2 = new DeliveryPackage(
+        List<DayOfWeek> netflixDays = new ArrayList<>();
+        for (DayOfWeek day : DayOfWeek.values()) {
+            netflixDays.add(day);
+        }
+
+        Subscription s2 = new Subscription(
                 2,
-                5.5,
-                "Turkestan 13",
-                "Mangilik El 35"
+                "netflix",
+                0,
+                LocalDate.of(2026, 2, 24),
+                LocalDate.of(2026, 3, 24),
+                netflixDays
         );
+        s2.recalcPrice(300);
+        subscriptions.add(s2);
 
-        Delivery d1 = new Delivery(pack1, cour1, cus1);
-        Delivery d2 = new Delivery(pack2, cour1, cus1);
-        d1.startDelivery();
-        d2.finishDelivery();
+        for (Order sub : subscriptions) {
+            sub.printOrder();
+        }
 
+        s1.extendSubscription(7);
+        s1.recalcPrice(500);
+        s1.printOrder();
 
-        DeliveryService delSer = new DeliveryService();
-        delSer.addDelivery(d1);
-        delSer.allDeliveries();
-
-        DeliveryHistory delHis = new DeliveryHistory();
-        delHis.addToHistory(d2);
-        delHis.showHistory();
-
-
-
-
+        s2.extendSubscription(10);
+        s2.recalcPrice(300);
+        s2.printOrder();
     }
 }
